a78fd712d849d84a94bdd7f3e540b1439e05a6e7
Lucas Fagundes <lucasfagundesvaz@gmail.com>
2025-03-14 09:34:27 -0300

    Atualização do código

---
+++ android/app/src/main/java/com/polimorfismo/calculadora/desafio_2_parte_1_calculadora_estrutura_inicial_flutter/MainActivity.java
@@ -0,0 +1,6 @@
+package com.polimorfismo.calculadora.desafio_2_parte_1_calculadora_estrutura_inicial_flutter;
+
+import io.flutter.embedding.android.FlutterActivity;
+
+public class MainActivity extends FlutterActivity {
+}
--- android/app/src/main/kotlin/com/desafio_calculadora_fase_1/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.desafio_calculadora_fase_1
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
